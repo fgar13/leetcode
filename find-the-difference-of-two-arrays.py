05b6b79fca9c7b3e25627b978260649a19a42c42
fgar13 <94147771+fgar13@users.noreply.github.com>
2023-05-18 18:28:14 -0400

    Includes better solution, found on LeetCode

--- find-the-difference-of-two-arrays.py
+++ find-the-difference-of-two-arrays.py
@@ -24,3 +24,7 @@ class Solution(object):
                 a2.append(ele)
         answer.append(a2)
         return answer
+
+    def findDifference2(self, nums1, nums2):
+        s1, s2 = set(nums1), set(nums2)
+        return [list(s1 - s2), list(s2 - s1)]
